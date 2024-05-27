fc556bb11b35c69c43a9d37acfe71b75623a0af6
arthur <arthur.broudoux@gmail.com>
2024-05-27 22:52:26 +0200

    refractor: update pokemon model

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -8,23 +8,57 @@ import (
 )
 
 var Pokedex []models.Pokemon
-var Counter int
 
 func InitPokedex() {
-	Counter = 1
-
 	pokemon1 := models.Pokemon{
 		Id: 1,
-		Name: "Bulbasaur",
-		Type: []string{"Grass", "Poison"},
-		Base: []int{45, 49, 49, 65, 65, 45},
+		Name: {
+			English: "Bulbasaur",
+			Japanese: "フシギダネ",
+			Chinese: "妙蛙种子",
+			French: "Bulbizarre",
+		},
+		Type: {"Grass", "Poison"},
+		Base: {
+			HP: 45,
+			Attack: 49,
+			Defense: 49,
+			SpAttack: 65,
+			SpDefense: 65,
+			Speed: 45,
+		},
 	}
 
 	Pokedex = append(Pokedex, pokemon1)
 }
 
-func GetPokedex(c * gin.Context) {
-	c.JSON(200, gin.H{"data": Pokedex})
+// func InitPokedex() {
+// 	file, err := os.Open("data/pokedex.json")
+
+// 	if err != nil {
+// 		fmt.Println(err, "Error opening file")
+// 		return
+// 	}
+
+// 	defer file.Close()
+
+// 	var pokedex []models.Pokemon
+// 	err = json.NewDecoder(file).Decode(&pokedex)
+
+// 	if err != nil {	
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	for _, pokemon := range pokedex {
+// 		fmt.Print("Pokemon: ", pokemon)
+// 	}
+
+// 	Pokedex = pokedex
+// }
+
+func GetPokedex(c *gin.Context) {
+c.JSON(200, gin.H{"pokedex": Pokedex})
 }
 
 func GetPokemon(c *gin.Context) {
--- src/models/models.go
+++ src/models/models.go
@@ -2,7 +2,19 @@ package models
 
 type Pokemon struct {
 	Id int `json:"id"`
-	Name string  `json:"name"`
+    Name struct {
+        English string `json:"english"`
+        Japanese string `json:"japanese"`
+        Chinese string `json:"chinese"`
+        French string `json:"french"`
+    } `json:"name"`
 	Type []string `json:"type"`
-	Base []int `json:"base"`
+    Base struct {
+        HP int `json:"HP"`
+        Attack int `json:"Attack"`
+        Defense int `json:"Defense"`
+        SpAttack int `json:"Sp.Attack"`
+        SpDefense int `json:"Sp.Defense"`
+        Speed int `json:"Speed"`
+    } `json:"base"`
 }
